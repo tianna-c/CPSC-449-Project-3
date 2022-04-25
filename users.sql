bc9a66da0860de682955f1e23d67b963b88fe282
Tianna Cano <39601543+tianna-c@users.noreply.github.com>
2022-04-25 13:33:16 -0700

    Add files via upload

--- users.sql
+++ users.sql
@@ -3,9 +3,9 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-    user_id INTEGER PRIMARY KEY,
+    user_id INTEGER,
     username VARCHAR UNIQUE,
-    uuid VARCHAR
+    uuid VARCHAR PRIMARY KEY
 );
 
 PRAGMA analysis_limit=1000;
