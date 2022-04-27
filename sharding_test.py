96845fe58182bbe142745cf32df40663c7e66407
Tianna Cano <39601543+tianna-c@users.noreply.github.com>
2022-04-27 12:12:39 -0700

    Add files via upload

--- sharding_test.py
+++ sharding_test.py
@@ -12,36 +12,28 @@ def create_uuid():
 	cur = con.cursor()
 	# creates uuid column in users table
 	try:
-		cur.execute("ALTER TABLE users ADD COLUMN uuid GUID") # it gives this error if you try running the file more than once, "sqlite3.OperationalError: duplicate column name: uuid", idk if we need to fix that later # maybe we can drop user_id and make uuid the primary key???
+		cur.execute("ALTER TABLE users ADD COLUMN uuid GUID") # it gives this error if you try running the file more than once, "sqlite3.OperationalError: duplicate column name: uuid"
 	except sqlite3.IntegrityError:
 		pass
+	# I think we should make a dictionary for the key-value pairs: user_id and uuid
 	user_id_count = {}
 	i = 1
+	# a for loop to generate uuid for each user and insert it into the uuid column
 	for i in range(NUM_USERS):
-		while True:
-			try:
-				user_uuid = uuid.uuid4()
-				user_id_count[i] = user_uuid
-				cur.execute("UPDATE users SET uuid = ? WHERE user_id = ?", [user_uuid, i])  #uuid is NULL
-				
-			except sqlite3.IntegrityError:
-				continue
-			i += 1
-			break
+		user_uuid = uuid.uuid4() # generate uuid
+		user_id_count[i] = user_uuid #insert uuid into dict according the the corresponding user_id from the loop
+		cur.execute("UPDATE users SET uuid = ? WHERE user_id = ?", [user_uuid, i])
+		i += 1
+	# creates uuid column in games tables
 	try:
 		cur.execute("ALTER TABLE games ADD COLUMN uuid GUID")
 	except sqlite3.IntegrityError:
 		pass
 	x = 0
+	# should do the same thing as the above loop
 	for x in range(NUM_STATS):
-	     while True:
-	     		try:
-	     			cur.execute("UPDATE games SET uuid = ? WHERE user_id = ?", [user_id_count[x], x]) #gives me an error: KeyError: 100000
-	     			
-	     		except sqlite3.IntegrityError:
-	     			continue
-	     		x += 1
-	     		break
+	     cur.execute("UPDATE games SET uuid = ? WHERE user_id = ?", [user_id_count[x], x]) #gives an error: KeyError: 100000
+	     x += 1
 	con.commit()
 create_uuid()
 
@@ -55,5 +47,3 @@ def sharding():
 	for i in range(3):
 		con_shard = sqlite3.connect('shard_' + str(i+1) + '.db')
 		#hash uuid/user_id and insert game data into corresponding shards
-		cur_two = con_shard.cursor()
-		cur_one.execute("SELECT * from games WHERE uuid % 3 = ?", [i])
